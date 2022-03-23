c31d2699983a04846abdcccc0f593f51f9bc5efb
nnphat6492 <41882530+nnphat6492@users.noreply.github.com>
2022-03-23 17:37:53 +0700

    Update laravel-sso.php

--- config/laravel-sso.php
+++ config/laravel-sso.php
@@ -23,7 +23,7 @@ return [
      |
      */
 
-    'usersModel' => \App\User::class,
+    'usersModel' => \App\Model\User::class,
     'brokersModel' => Zefy\LaravelSSO\Models\Broker::class,
 
     // Table used in Zefy\LaravelSSO\Models\Broker model
